9da4f3a0ad430315f0f6bf3d0bffa942295b3b9a
satoudesita <167180203+satoudesita@users.noreply.github.com>
2024-10-29 09:08:17 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,6 @@ import streamlit.components.v1 as components
 st.set_page_config(
      page_title="study motivation App",
      page_icon="📖",
-     layout="wide",
      initial_sidebar_state="expanded",
      menu_items={
     'Get Help': 'https://www.extremelycoolapp.com/help',
